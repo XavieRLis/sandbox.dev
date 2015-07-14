c529a8e1bbb07b1744f2d1f31fb6b1250685f307
XavieR.Lis <xavier.lis@gmail.com>
2015-07-14 22:01:07 +0300

    Add some settings for FOSUserBundle

---
+++ src/Xavierlis/UserBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Xavierlis\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+
+/**
+ * User
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
